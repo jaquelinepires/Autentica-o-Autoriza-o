ef9bafceeba48db40a4f769aea8095b28927de34
Jaqueline <piresjaqueline17@gmail.com>
2022-01-03 21:50:20 -0300

    Performing automatic logout

--- contexts/AuthContext.tsx
+++ contexts/AuthContext.tsx
@@ -1,5 +1,5 @@
 import { createContext, ReactNode, useEffect, useState  } from "react";
-import { setCookie, parseCookies } from 'nookies'
+import { setCookie, parseCookies, destroyCookie } from 'nookies'
 import  Router  from "next/router"
 import { api } from "../services/api";
 
@@ -23,6 +23,12 @@ type AuthProviderProps = {
 }
 
 export const AuthContext = createContext({} as AuthContextData)
+export function signOut() {
+  destroyCookie(undefined, 'nextauth.token')
+  destroyCookie(undefined, 'nextauth.refreshToken')
+
+  Router.push('/')
+}
 
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<User>()
@@ -30,11 +36,17 @@ export function AuthProvider({ children }: AuthProviderProps) {
 
   useEffect(() => {
     const { 'nextauth.token': token } = parseCookies()
+
     if (token) {
-      api.get('/me').then(response => {
+      api.get('/me')
+      .then(response => {
         const { email, permissions, roles } = response.data
 
         setUser({email, permissions, roles})
+      })
+      .catch(() => {
+        signOut();
+
       })
     }
   }, [])
--- services/api.ts
+++ services/api.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError } from 'axios'
 import { parseCookies, setCookie } from 'nookies'
+import { signOut } from '../contexts/AuthContext';
 
 let cookies = parseCookies();
 let isRefreshing = false;
@@ -64,7 +65,8 @@ api.interceptors.response.use(response => {
         })
       })
     } else {
-     // desogar o usuario
+     signOut();
   }
 }
+  return Promise.reject(error);
 })
